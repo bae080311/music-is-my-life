2c36c7a09215754054c70c15147616bb25002e4f
bae080311 <s24024@gsm.hs.kr>
2025-02-15 13:34:40 +0900

    Feat: login style

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Callback from "../pages/callback";
+import Login from "../pages/login/login";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/callback" element={<Callback />} />
       </Routes>
     </BrowserRouter>
