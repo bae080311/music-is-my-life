eb351fa48813c851a8c3dd53086b73e1c9b2e297
bae080311 <s24024@gsm.hs.kr>
2025-02-16 17:15:53 +0900

    Feat: header

---
+++ src/shared/ui/header/header.tsx
@@ -0,0 +1,16 @@
+import * as S from "./styles";
+
+const Header = () => {
+  return (
+    <S.Header>
+      {/*logoicon*/}
+      <div>
+        {/*homeicon*/}
+        <input type="text" />
+      </div>
+      {/*profileicon*/}
+    </S.Header>
+  );
+};
+
+export default Header;
