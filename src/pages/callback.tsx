1e36949b66f1f5f6e52b7327279f7cb61796a575
bae080311 <s24024@gsm.hs.kr>
2025-02-17 20:04:20 +0900

    Feat: set token

--- src/pages/callback.tsx
+++ src/pages/callback.tsx
@@ -1,7 +1,14 @@
+import { useSearchParams } from "react-router-dom";
 import Footer from "../shared/ui/footer";
 import Header from "../shared/ui/header/header";
+import { useEffect } from "react";
 
 const Callback = () => {
+  const [SearchParams] = useSearchParams();
+  useEffect(() => {
+    const code = SearchParams.get("code");
+    code && localStorage.setItem("code", code);
+  });
   return (
     <div>
       <Header />
