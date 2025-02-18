66d03d6b29a8f21e29461e2c94fe960eb87caf8a
bae080311 <s24024@gsm.hs.kr>
2025-02-19 00:26:39 +0900

    Remove import

--- src/pages/callback.tsx
+++ src/pages/callback.tsx
@@ -1,42 +1,24 @@
 import { useSearchParams } from "react-router-dom";
 import Footer from "../widgets/footer";
 import Header from "../widgets/header/header";
-import { useEffect, useState } from "react";
-import * as F from "../shared/utils/api/music/music";
-import { Album, Artist, AudioBook } from "../entities/music";
-import List from "../shared/ui/list/list";
+import { useEffect } from "react";
 import { token } from "../features/auth/getToken";
 
 const Callback = () => {
   const [SearchParams] = useSearchParams();
-  const [audioBooks, setAudioBooks] = useState<AudioBook[]>();
-  const [artists, setArtists] = useState<Artist[]>();
-  const [albums, setAlbums] = useState<Album[]>();
 
   useEffect(() => {
-    const code = SearchParams.get("code");
-    code && token(code);
-
-    const fetchDatas = async () => {
-      const fetchAudioBooks = await F.getAudioBooks();
-      fetchAudioBooks.success && setAudioBooks(fetchAudioBooks.data);
-
-      const fetchArtists = await F.getArtists();
-      fetchArtists.success && setArtists(fetchArtists.data);
-
-      const fetchAlbums = await F.getAlbums();
-      fetchAlbums.success && setAlbums(fetchAlbums.data);
-    };
+    if (!localStorage.getItem("spotify_access_token")) {
+      const code = SearchParams.get("code");
+      code && token(code);
+    }
+    const fetchDatas = async () => {};
     fetchDatas();
   }, []);
   return (
     <div>
       <Header />
-      <main>
-        {audioBooks && <List items={audioBooks} name="AudioBook" />}
-        {artists && <List items={artists} name="Artist" />}
-        {albums && <List items={albums} name="Album" />}
-      </main>
+      <main></main>
       <Footer />
     </div>
   );
